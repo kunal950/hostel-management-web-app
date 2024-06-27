2a262f32d139420d0df4d86ebef48920d1245324
Kunal Agarwal <kunalagarwal9204@gmail.com>
2024-06-27 19:27:04 +0530

    all setup for the app is done

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,32 @@
 import express from "express";
 import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+
+const app = express();
+config({ path: "./config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
 
 dbConnection();
 
